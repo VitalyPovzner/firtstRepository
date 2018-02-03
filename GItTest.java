03b992f18113c15b3e2d4b35c6b92e8c428d5a8c
Vitaliy <vitaliypovzner@gmail.com>
2018-02-03 16:56:09 +0200

    Random +array

--- GItTest.java
+++ GItTest.java
@@ -1,8 +1,11 @@
+import java.util.Random;
 
 public class GItTest {
 
 	public static void main(String[] args) {
 		System.out.println("tst");
+		Random r=new Random();
+		int[]arr=new int[10];
 	}
 
 }
