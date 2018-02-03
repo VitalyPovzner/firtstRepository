0821d50a99d813f88bc5fd6199ebd5afbe76e453
Vitaliy <vitaliypovzner@gmail.com>
2018-02-03 16:57:15 +0200

    created cicle over array

--- GItTest.java
+++ GItTest.java
@@ -6,6 +6,9 @@ public class GItTest {
 		System.out.println("tst");
 		Random r=new Random();
 		int[]arr=new int[10];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i]=r.nextInt(10);
+		}
 	}
 
 }
